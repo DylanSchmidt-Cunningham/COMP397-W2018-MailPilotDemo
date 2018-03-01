046e93c4000b6f7539a70108c09cbc17cda25940
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-03-01 01:59:22 -0500

    added island to the scene

--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -3,6 +3,7 @@ module scenes {
         // Private Instance Variables
         private _ocean:objects.Ocean;
         private _plane:objects.Plane;
+        private _island:objects.Island;
 
         // Public Properties
 
@@ -22,18 +23,23 @@ module scenes {
         public Start():void {
             this._ocean = new objects.Ocean(this.assetManager);
             this._plane = new objects.Plane(this.assetManager);
+            this._island = new objects.Island(this.assetManager);
             this.Main();
         }
 
         public Update():void {
             this._ocean.Update();
             this._plane.Update();
+            this._island.Update();
         }
 
         // This is where the fun happens
         public Main():void {
             // add the ocean to the scene
             this.addChild(this._ocean);
+
+            // add the island to the scene
+            this.addChild(this._island);
 
             // add the plane to the scene
             this.addChild(this._plane);
